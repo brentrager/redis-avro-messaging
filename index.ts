9a9a01b7a5d688a991e09aa482a6fff8f069d956
Brent Rager <brent.rager@gmail.com>
2018-09-08 18:24:50 -0400

    Added avro payload building and parsing.

--- index.ts
+++ index.ts
@@ -1,9 +1,7 @@
-import * as avsc from 'avsc';
 import Redis from 'ioredis';
-import * as url from 'url';
 import * as uuid from 'uuid';
 import * as EventEmitter from 'events';
-import * as _ from 'lodash';
+import _ from 'lodash';
 // tslint:disable-next-line:variable-name no-require-imports
 const ContainerLogging = require('@pureconnect/containerlogging');
 // tslint:disable-next-line:variable-name no-require-imports
@@ -49,6 +47,83 @@ class ExpiringPromise {
         const expiringPromise = new ExpiringPromise(promise, timeoutMs);
 
         return expiringPromise.wait();
+    }
+}
+
+class AvroSchemaWithId {
+    constructor(private _id: number, private _schema: any) {
+    }
+
+    schema(): any {
+        return this._schema;
+    }
+
+    id(): number {
+        return this._id;
+    }
+}
+
+class NotificationProtocol {
+    private protocolVersion = 0;
+    private offsets = {
+        protocolVersion: 0,
+        schemaId: 1,
+        data: 5
+    };
+
+    constructor(private avroSchemaCacheManager: AvroSchemaCacheManager) {
+    }
+
+    /**
+     * Build a payload (either a key or value for a notification).  Returns a Buffer containing the
+     * payload.
+     */
+    buildPayload(schemaId: number, avroType: any, data: any): Buffer {
+        const dataLen = icAvroLib.getAvroLength(avroType, data);
+        const payload = Buffer.allocUnsafe(this.offsets.data + dataLen);
+
+        payload.writeUInt8(this.protocolVersion, this.offsets.protocolVersion);
+        payload.writeUInt32BE(schemaId, this.offsets.schemaId);
+        icAvroLib.avroSerialize(avroType, payload, this.offsets.data, dataLen, data);
+
+        return payload;
+    }
+
+    /**
+     * Parse a payload (either a key or value from a notification).  Retrieves the schema using
+     * schemaCache and parses the payload.
+     *
+     * If the protocol version is not supported, the promise is rejected.
+     *
+     * @param payload - A Buffer holding the key or value payload from the notification
+     * @param schemaCache - A SchemaRegistryCache used to retrieve the sender's schema
+     * @return Promise - an AvroReceivePayload that can be used to deserialize the data
+     */
+    async parsePayload(payload: Buffer, nodeId: string): Promise<Buffer> {
+        if (!payload) {
+            throw new Error('Cannot parse null payload');
+        }
+        if (payload.length < this.offsets.data) {
+            throw new Error(`Payload size ${payload.length} is invalid`);
+        }
+
+        // Read the protocol version
+        const msgProtocolVersion = payload.readUInt8(this.offsets.protocolVersion);
+        // If the protocol version is not supported, reject the promise
+        if (msgProtocolVersion !== this.protocolVersion) {
+            throw new Error(`Payload uses unsupported protocol version ${msgProtocolVersion}`);
+        }
+
+        // Read the schema ID
+        const schemaId = payload.readUInt32BE(this.offsets.schemaId);
+        // Slice off the data section
+        const dataBuf = payload.slice(this.offsets.data);
+
+        const schema = await this.avroSchemaCacheManager.getSchema(nodeId, schemaId);
+
+        const dataType = icAvroLib.getParsedType(schema);
+
+        return new icAvroLib.AvroReceivePayload(dataBuf, dataType);
     }
 }
 
@@ -120,18 +195,18 @@ class AvroSchemaCache {
      * Each service will keep an array of schemas it writes. The schema ID is the position of that schema
      * in the array.
      */
-    private cache: Map<string, Map<number, any>> = new Map();
+    private cache: Map<string, Map<number, AvroSchemaWithId>> = new Map();
 
     constructor() {
     }
 
     setSchemas(nodeId: string, schemas: Array<any>): void {
         this.cache.set(nodeId, schemas.reduce((result, schema, index) => {
-            result.set(index, schema);
+            result.set(index, new AvroSchemaWithId(index, schema));
         }, new Map()));
     }
 
-    getSchema(nodeId: string, schemaId: number): any {
+    getSchema(nodeId: string, schemaId: number): AvroSchemaWithId {
         let result: any;
         const schemaMap = this.cache.get(nodeId);
         if (schemaMap) {
@@ -172,7 +247,7 @@ class AvroSchemaCacheManager extends EventEmitter {
         this.schemaCache.setSchemas(nodeId, schemas);
     }
 
-    async getSchema(nodeId: string, schemaId: number): Promise<any> {
+    async getSchema(nodeId: string, schemaId: number): Promise<AvroSchemaWithId> {
         let result = this.schemaCache.getSchema(nodeId, schemaId);
 
         if (!result) {
@@ -209,10 +284,15 @@ class AvroSchemasProducedManager {
      * This class keeps track of all the schemas for which this microservice produces messages. This could be
      * either produced notifications or requests made to other microservies and the expected responses.
      */
-    private schemasProduced: Array<any>;
+    private schemasProduced: Array<AvroSchemaWithId>;
 
-    private async notifySchemasProduced(): Promise<void> {
-        return this.redisPubSub.publish(AVRO_SCHEMA_REGISTER_CHANNEL, this.schemasProduced);
+    private async notifySchemasProduced(): Promise<Array<any>> {
+        const schemas = this.schemasProduced.map(x => x.schema());
+        await this.redisPubSub.publish(AVRO_SCHEMA_REGISTER_CHANNEL, schemas);
+
+        log.verbose(`Notified other services of ${schemas.length} schemas produced.`);
+
+        return schemas;
     }
 
     constructor(private redisPubSub: RedisPubSub) {
@@ -232,13 +312,44 @@ class AvroSchemasProducedManager {
             log.verbose('Listening for schema register requests.');
         });
     }
-    async addProducedSchemas(...schemas: Array<any>): Promise<Array<number>> {
+
+    async addProducedSchemas(...schemas: Array<any>): Promise<Array<AvroSchemaWithId>> {
+        const existingSchemasWithId = [];
+        const schemasWithId = [];
+        let index = this.schemasProduced.length;
+        for (const schema of schemas) {
+            const schemaWithId = this.schemasProduced.find(_schemaWithId => {
+                return _.isEqual(_schemaWithId.schema, schema);
+            });
+            if (schemaWithId) {
+                existingSchemasWithId.push(schemaWithId);
+            } else {
+                schemasWithId.push(new AvroSchemaWithId(index, schema));
+                index++;
+            }
+        }
         this.schemasProduced.concat(schemas);
 
         await this.notifySchemasProduced();
 
         // Return an array of schema IDs that represent the newly added schemas
-        return _.range(this.schemasProduced.length - schemas.length, this.schemasProduced.length - 1);
+        return existingSchemasWithId.concat(schemasWithId);
+    }
+}
+
+class AvroProducer {
+    private schemaWithId: AvroSchemaWithId;
+    private readyPromise: Promise<void>;
+
+    constructor(private redisPubSub: RedisPubSub, private avroSchemaCacheManager: AvroSchemasProducedManager, schema: any) {
+        this.readyPromise = new Promise(async (resolve, reject) => {
+            this.schemaWithId = (await this.avroSchemaCacheManager.addProducedSchemas(schema))[0];
+            resolve();
+        });
+    }
+
+    async produce(message: any): Promise<void> {
+        await this.readyPromise;
     }
 }
 
@@ -251,7 +362,7 @@ export class RedisAvroMessaging {
         this.avroSchemaCacheManager = new AvroSchemaCacheManager(this.redisPubSub);
     }
 
-    async createAvroProducer(): Promise<void> {
+    async createAvroProducer(schema: any): Promise<void> {
 
     }
 
